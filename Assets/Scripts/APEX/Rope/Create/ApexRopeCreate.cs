b47c7e9dd9d518fc4653f6eb457175da4f6ffa79
Mustenaka <mumten120@outlook.com>
2023-11-20 23:24:08 +0800

    add rope create component

--- Assets/Scripts/APEX/Rope/Create/ApexRopeCreate.cs
+++ Assets/Scripts/APEX/Rope/Create/ApexRopeCreate.cs
@@ -1,7 +1,44 @@
-namespace APEX.Rope.Create
+using UnityEngine;
+
+namespace APEX.Rope
 {
-    public class ApexRopeCreate
+    public class ApexRopeCreate : MonoBehaviour
     {
+        public bool useSelfPosition;
+        public Vector3 firstParticlePosition = Vector3.zero;
+        public int particleCount = 10;
+        public float stepSize = 1.2f;
+        public Vector3 stepDirect = Vector3.left;
+
+        public GameObject obj;
         
+        private void Start()
+        {
+            // if the obj is empty, create sphere to fill it
+            if (obj == null)
+            {
+                obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            }
+            
+            // is use self position, the firstParticlePosition is self
+            if (useSelfPosition)
+            {
+                firstParticlePosition = this.transform.position;
+            }
+            
+            InitRope();
+        }
+
+        private void InitRope()
+        {
+            for (int i = 0; i < particleCount; i++)
+            {
+                Vector3 particlePosition = firstParticlePosition + i * (stepSize * stepDirect);
+                
+                var particle = GameObject.Instantiate(obj, transform, true);
+                particle.name = i.ToString();
+                particle.transform.position = particlePosition;
+            }
+        }
     }
 }
