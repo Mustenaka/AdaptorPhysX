3f0cc980620b5e61162f22988bbf27e41ac22f16
Mustenaka <mumten120@outlook.com>
2024-02-26 18:38:42 +0800

    adjust

--- Assets/Scripts/APEX/Common/Constraints/Brust/PinConstraintJob.cs
+++ Assets/Scripts/APEX/Common/Constraints/Brust/PinConstraintJob.cs
@@ -5,5 +5,10 @@ namespace APEX.Common.Constraints
     public struct ApexPinConstraint
     {
         public float3 position;
+
+        public ApexPinConstraint(float3 position)
+        {
+            this.position = position;
+        }
     }
 }
