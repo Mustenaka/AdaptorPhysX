d405f79c05c75fc32a9f987ab744f0d32c29d027
Mustenaka <mumten120@outlook.com>
2024-02-27 14:22:26 +0800

    clean

--- Assets/Scripts/APEX/Common/Solver/Type/EApexSolverBackend.cs
+++ Assets/Scripts/APEX/Common/Solver/Type/EApexSolverBackend.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace APEX.Common.Solver
 {
     /// <summary>
     /// Declares which platform the calculation runtime
     /// </summary>
+    [Obsolete]
     public enum EApexSolverBackend
     {
         SingleThread, 
